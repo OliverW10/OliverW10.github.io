b301348b2bbda03ba5ef1b861e79da8d04a6441a
oliverw10 <oliver@warrickfamily.com>
2022-10-30 19:23:03 +1100

    Update project descriptions and dates

--- components/projectData.ts
+++ components/projectData.ts
@@ -20,18 +20,18 @@ export const allProjects: CardInfo[] = [
   {
     title: "Robotic Arm",
     info: "Homemade 3DOF robot arm",
-    more_info: "Runs on a raspberry pi using the Orocos library for inverse kinematics and trajectory planning, uses apriltags and a intel realsense camera to localize",
+    more_info: "A small handheld robotic arm that uses a intel realsens and apriltags to position keep a end effector still while the arms base is moved.",
     repo: "https://github.com/OliverW10/arm",
     video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     completed: false,
-    date: new Date(2022, 8),
-    coolness: 4, // 0-10
+    date: new Date(2022, 10),
+    coolness: 5, // 0-10
     language: "C/C++",
   },
   {
     title: "Word search solver",
     info: "An android app to solve word search's (find-a-word's) by taking a picutre",
-    more_info: "Uses Kivy to build to android, uses opencv to do image processing",
+    more_info: "Uses Kivy to build to android, uses opencv to do image processing. Used the K-Nearest-Neighbor algorithm to classify the letters with over 95% accuracy.",
     repo: "https://github.com/OliverW10/word-search-solver",
     live: "https://OliverW10.github.io/word-search-solver",
     download: "https://github.com/OliverW10/word-search-solver/releases/download/v0.3-alpha/myapp-0.3-armeabi-v7a-debug.apk",
@@ -43,21 +43,21 @@ export const allProjects: CardInfo[] = [
   {
     title: "Bad Wordle Solver",
     info: "Program to a solve wordle by only seeing the colors of the squares",
-    more_info: "Tries to guess the wordle by only seeing the emoji board you get when you finish a game",
+    more_info: "Tries to guess the wordle by only seeing the emoji board you get when you finish a game that only shows the colour of each square. It turns out that you need a large number of these boards to get a distinct answer but it could narrow it down to a dozen or so words with just a few.",
     repo: "https://github.com/OliverW10/wordle",
     completed: true,
-    date: new Date(2021, 11),
+    date: new Date(2022, 2),
     coolness: 3, // 0-10
     language: "C/C++",
   },
   {
     title: "Pacman",
     info: "AI experiments in pacman",
-    more_info: "Pacman made using pygame with various AI's to control the ghosts and pacman",
+    more_info: "Pacman made using pygame with various AI's to control both the ghosts and pacman. Some of the implimented AI's were: game tree search, imitation and adversarial(incomplete) machine learning, heuristic algorithms, recreation of origonal game's ghost AI.",
     repo: "https://github.com/OliverW10/pacman",
     live: "https://OliverW10.github.io/pacman",
     completed: false,
-    date: new Date(2022, 6),
+    date: new Date(2022, 5),
     coolness: 5, // 0-10
     language: "Python",
   },
@@ -79,18 +79,18 @@ export const allProjects: CardInfo[] = [
     repo: "https://github.com/OliverW10/chaos",
     live: "https://OliverW10.github.io/chaos/webgl",
     completed: true,
-    date: new Date(2022, 1),
+    date: new Date(2022, 0),
     coolness: 3, // 0-10
     language: "Other",
   },
   {
     title: "Playlister",
-    info: "Sorts spotify playlists by the colour of their album cover",
-    more_info: "Used react and the spotify api",
+    info: "Sorts spotify playlists by the colour of their album cover and can combine playlists.",
+    more_info: "Used react and the spotify api. Not in particularly finished state.",
     repo: "https://github.com/OliverW10/playlister",
     live: "https://oliverw10.github.io/playlister",
     completed: false,
-    date: new Date(2021, 7),
+    date: new Date(2021, 6),
     coolness: 3, // 0-10
     language: "Javascript",
   },
@@ -101,7 +101,7 @@ export const allProjects: CardInfo[] = [
     repo: "https://github.com/OliverW10/js-games/blob/games/tennis",
     live: "https://oliverw10.github.io/js-games/tennis/index.html",
     completed: true,
-    date: new Date(2020, 4),
+    date: new Date(2020, 6),
     coolness: 5, // 0-10
     language: "Javascript",
   },
@@ -119,23 +119,23 @@ export const allProjects: CardInfo[] = [
   {
     title: "Multiplayer",
     info: "Realtime multiplayer pvp web game using WebRTC",
-    more_info: "Uses react for the UI and WebRTC for peer-to-peer aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa ",
+    more_info: "Uses react for the UI and WebRTC for peer-to-peer networking.",
     repo: "https://github.com/OliverW10/multiplayer",
     live: "https://OliverW10.github.io/multiplayer",
     completed: false,
-    date: new Date(2021, 11),
+    date: new Date(2021, 8),
     coolness: 4, // 0-10
     language: "Javascript",
   },
   {
-    title: "FRC 2022",
-    info: "Captain for the 2022 First robotics competition season on team 4774",
-    more_info: "Lead a team to build a robot and automate almost all robot actions using RobotPy",
+    title: "First robotics competition",
+    info: "Captain on team 4774 'The Drop Bears' for the 2022-3 seasons.",
+    more_info: "Came 2nd at the australian regional and won the hawaii regional.",
     repo: "https://github.com/thedropbears/pyrapidreact",
     video: "https://www.youtube.com/watch?v=IaR-7Fr0wzE",
     completed: true,
     date: new Date(2022, 2),
-    coolness: 6, // 0-10
+    coolness: 5, // 0-10
     language: "Python",
   },
 ]
